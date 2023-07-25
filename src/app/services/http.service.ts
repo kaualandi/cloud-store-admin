2c46838a7a418a99ec56b49049a829b7340e43c1
Kauã Landi <eu@kaualf.com>
2023-07-25 20:41:05 -0300

    feat: added new server base_url

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -44,19 +44,19 @@ export class HttpService {
     return headers;
   }
 
-  private handleError(error: HttpErrorResponse) {
+  private handleError = (error: HttpErrorResponse) => {
     let errorMessage = '';
     if (error.error instanceof ErrorEvent) {
       // Erro ocorreu no lado do client
       errorMessage = error.error.message;
     } else {
       // Erro ocorreu no lado do servidor
-      errorMessage = `Erro: ${error.status}: ${error.error.message}`;
+      errorMessage = error.error.message;
     }
     this.snackbar.error(errorMessage);
 
     return throwError(() => errorMessage);
-  }
+  };
 
   /**
    * ### Método GET
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: '',
+  base_url: 'https://cloud-api.kaualf.com/',
   page_size: 10,
 };
